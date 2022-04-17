9e48c58e0b079ad25a52fcb9064702116be4cec9
Dias <l1verpoolg24@gmail.com>
2022-04-17 19:05:32 +0600

    feat:completed functions or changefieldset, controlfield, router, search, upload, servicebook

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,22 @@
-import Navigo from 'navigo';
+import {initRouter} from './modules/router.js';
+import './modules/search.js';
+import changeFieldset from "./modules/changeFieldset.js";
+import {controlField} from "./modules/controlField.js";
+import './modules/upload.js'
 
-const library = document.querySelector('.library');
-const book = document.querySelector('.book');
-const add = document.querySelector('.add');
-const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
-const backBtn = document.querySelectorAll('.header__btn_back');
+const fieldsBtnSort = document.querySelector('.fields__btn_sort');
+const fieldsListSort = document.querySelector('.fields__list_sort');
+const fieldsBtnFilter = document.querySelector('.fields__btn_filter');
+const fieldsListFilter = document.querySelector('.fields__list_filter');
 
-
-const router = new Navigo('/', {
-  hash: true,
-});
-
-const closeAllPage = () => {
-  library.classList.add('hidden');
-  book.classList.add('hidden');
-  add.classList.add('hidden');
-}
-
-router.on({
-  '/': () => {
-    closeAllPage();
-    library.classList.remove('hidden');
-  },
-  'book': () => {
-    closeAllPage();
-    book.classList.remove('hidden');
-  },
-  'add': () => {
-    closeAllPage();
-    add.classList.remove('hidden');
-  }
-}).resolve();
-
-addBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    router.navigate('add');
-  })
-});
-
-backBtn.forEach(btn => {
-  btn.addEventListener('click', () => {
-    router.navigate('/');
-  })
-})
+const init = () => {
+  initRouter();
 
 
+  controlField(fieldsBtnSort, fieldsListSort, fieldsListFilter);
+  controlField(fieldsBtnFilter, fieldsListFilter, fieldsListSort);
+
+  changeFieldset();
+};
+
+init();
---
+++ src/js/modules/renderBook.js
@@ -0,0 +1,6 @@
+import {getLabels, getBooks, API_URI} from "./serviceBook.js";
+
+export const renderBook = async (id) => {
+  const [books, labels] = await Promise.all([getBooks(id), getLabels()]);
+
+};
